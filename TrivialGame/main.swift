3e80c14aaf16ec87f9062f67b2f200daeef1caaa
Lhais Silva <lhaisrodrigues@172-16-3-29.0000.ufpe.br.iosda.org>
2017-10-06 10:04:07 -0300

    Initial Commit

---
+++ TrivialGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TrivialGame
+//
+//  Created by Lhais Rodrigues Silva on 06/10/17.
+//  Copyright © 2017 Lhais Rodrigues Silva. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
